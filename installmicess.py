410ccd77181f745e9d6332a6753e9f3ae6388ea6
betmen N dgeng <betmen.dlx@gmail.com>
2025-03-18 14:54:00 +0800

    Update installmicess.py

--- installmicess.py
+++ installmicess.py
@@ -294,7 +294,7 @@ def main():
         "Freq": "git clone https://github.com/takshal/freq.git && cd freq && pip install .",
         "Gargs": "go install -v github.com/brentp/gargs@latest",
         "Gau": "go install -v github.com/lc/gau@latest",
-        "Ghauri": "git clone https://github.com/r0oth3x49/ghauri.git ; cd ghauri ; python3 -m pip install --upgrade -r requirements.txt && python3 -m pip install -e .",
+        "Ghauri": "git clone https://github.com/r0oth3x49/ghauri.git && cd ghauri && python3 -m pip install --upgrade -r requirements.txt && python3 -m pip install -e .",
         "Gf": "go install -v github.com/tomnomnom/gf@latest",  # Ditangani khusus
         "Gxss": "go install github.com/KathanP19/Gxss@latest",
         "Github-Search": "git clone https://github.com/gwen001/github-search.git && cd github-search && pip install -r requirements3.txt",
